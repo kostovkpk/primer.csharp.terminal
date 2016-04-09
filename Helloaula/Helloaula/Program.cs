0f3d05cfe374d685626af4b7d788cf37c7bccd45
Калоян Костов <kostovkpk@gmail.com>
2016-04-09 20:18:14 +0300

    създаване на програма hello aula

---
+++ Helloaula/Helloaula/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Helloaula
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello aula!");
+			Console.ReadKey ();
+		}
+	}
+}
