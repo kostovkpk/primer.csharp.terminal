d21af4dd683790eefdd79f1bff0c2b2229a2c20a
Калоян Костов <kostovkpk@gmail.com>
2016-04-09 20:59:42 +0300

    Модул02 добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниране на променливи
+
+			//Събиране на числа
+
+			//Писане в конзолата
+		}
+	}
+}
