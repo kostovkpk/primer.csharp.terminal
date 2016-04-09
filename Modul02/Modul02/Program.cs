f4bd33393d0f6b0f79d9727a890f6e3e819193d3
Калоян Костов <kostovkpk@gmail.com>
2016-04-09 21:39:17 +0300

    модул02, видео02 - променливи от тип int

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -7,10 +7,19 @@ namespace Modul02
 		public static void Main (string[] args)
 		{
 			//Дефиниране на променливи
+			int a = 0, b = 0;
+			int sum = 0;
 
 			//Събиране на числа
+			a=5; b=55;
+			sum = a + b;
 
 			//Писане в конзолата
+			Console.Write (a);
+			Console.Write (" + ");
+			Console.Write (b);
+			Console.Write (" = ");
+			Console.WriteLine ( sum );
 		}
 	}
 }
